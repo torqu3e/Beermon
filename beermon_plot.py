e17406d5ddce1cd9868f6959c6941b7ddc78462d
Tejinder Singh <github@torqu3e.net>
2019-04-09 20:08:59 -0400

    Replaced ugly file parsing logic to CSV reader and pandas dataframe for plotting. Savgol_filter is unavailable with pandas hence using rolling window mean. Window value should be tweaked to suit preference of plot noise. Failed temperature values written as NaN instead of -1 Dropped microsecond precision to save 7 bytes a line.

--- beermon_plot.py
+++ beermon_plot.py
@@ -7,30 +7,21 @@ import matplotlib.pyplot as pp
 import numpy as np
 import datetime
 import threading
-from scipy.signal import savgol_filter
-
+import pandas as pd
 
 URL = 'http://192.168.2.55/' #Temperature sensor endpoint
 FILENAME = 'ts_temp.txt'    #file to store temperature data
 
 def plot():
-    with open(FILENAME, 'r') as f:
-        #Converting column 1 to dateobject where temperature is not logged as -1 (error condition)
-        ts = [datetime.datetime.fromtimestamp(float(x.split(' ')[0])) for x in f.readlines() if float(x.split(' ')[1]) >= 0]
-        f.seek(0) #ugly
-        cur_temp = [y.split(' ')[1] for y in f.readlines() if float(y.split(' ')[1]) >= 0]
-        #Savgol filter to smooth the noise from sensor data, window and polynomial setting are tweakable
-        smoothed_temp = savgol_filter(cur_temp, 131, 3)
-    pp.figure(figsize=(30,21))
-    pp.title('Frementor Temperature',fontsize=32)
+    df = pd.read_csv(FILENAME,sep=' ', header=None, names=['Timestamp','Temperature'], parse_dates=['Timestamp'], date_parser=lambda epoch: pd.to_datetime(epoch, unit='s')).set_index('Timestamp').interpolate().rolling(window=131,center=True).mean()
+    df.plot(kind='line',y='Temperature',figsize=(30,21),linewidth=4,fontsize=20,rot=45)
     pp.ylabel(u'\xb0C',fontsize=24)
-    pp.yticks(fontsize=20)
-    pp.xticks(rotation=270,fontsize=20)
-    pp.xlabel('Date-hour',fontsize=24)
-    pp.plot(ts,smoothed_temp,linewidth=4) #replace smoothed_temp to cur_temp to see noisy plot
+    pp.xlabel('Timestamp',fontsize=24)
+    pp.title(label='Fermentor Temperature',fontsize=32)
     pp.show()
 
 def main():
+    #threading to request data in the background while generating plot
     t = threading.Thread(target=readtemp)
     t.start()
     plot()
@@ -40,15 +31,15 @@ def readtemp():
     try:
         r = requests.get(URL)
     except:
-        cur_temp = -1
+        cur_temp = np.nan
     else:
         soup = BeautifulSoup(r.text, 'html.parser')
         cur_temp = soup.b.string[0:5]
     finally:
-        cur_time = time.time()
+        cur_time = int(time.time()) #dropping resolution to 1s, saving 7 bytes a line
         with open(FILENAME, 'a') as open_file:
             open_file.write(str(cur_time) + ' ' + str(cur_temp) + '\n')
-        print('Fermentation vessel temperature at', cur_time, 'is', cur_temp, '*C')
+        print('Fermentation vessel temperature at', cur_time, 'is', cur_temp, u'\xb0C')
 
 
 if __name__ == "__main__":
