ca02b06277682c66c92594a31047382475a4f564
Tejinder Singh <github@torqu3e.net>
2019-05-23 11:11:22 -0400

    Lynting/cleaning up

--- beermon_plot.py
+++ beermon_plot.py
@@ -12,15 +12,24 @@ from bokeh.models import ColumnDataSource, HoverTool
 URL = 'http://192.168.1.18/' #Temperature sensor endpoint
 FILENAME = 'ts_temp.txt'    #file to store temperature data
 
+
 def genframe():
-    """Generates the pandas dataframe from the csv file containing temperature data and returns last temperature reading for comparison to update file"""
+    """
+    Generates dataframe from CSV file containing temperature data
+    Returns a smoothed data frame and last temperature reading
+    """
     data_frame = pd.read_csv(FILENAME, sep=' ', header=None, names=['Timestamp', 'Temperature'], parse_dates=['Timestamp'], date_parser=lambda epoch: pd.to_datetime(epoch, unit='s'))
-    smoothed_df = data_frame.set_index('Timestamp').interpolate().rolling(window=len(data_frame)//17+1).mean() #Experimentally determined window length to provide appropriate smoothing of the graph, +1 is for conditions where the data frame does not contain enough data hence setting window size to 1
+    #Experimentally determined window length to provide appropriate smoothing of the graph, 
+    # +1 is for conditions where the data frame does not contain enough data hence setting window size to 1
+    smoothed_df = data_frame.set_index('Timestamp').interpolate().rolling(window=len(data_frame)//17+1).mean() 
     prev_temp = data_frame['Temperature'].iloc[-1]
     return(smoothed_df, prev_temp)
 
+
 def plot(data_frame):
-    """Plots a bokeh chart from the generated dataframe"""
+    """
+    Plots a bokeh chart from the generated dataframe
+    """
     data_frame = data_frame
     source = ColumnDataSource(data_frame)
     output_file("output.html")
@@ -47,9 +56,12 @@ def main():
     readtemp_thread.start()
     plot(data_frame)
 
+
 # Reading temp in function to run it in thread because GUI needs to be run in main thread
 def readtemp(prev_temp):
-    """Reads temperature from the ESP8266 endpoint"""
+    """
+    Reads temperature from the ESP8266 endpoint
+    """
     prev_temp = prev_temp
     try:
         req_page = requests.get(URL)
